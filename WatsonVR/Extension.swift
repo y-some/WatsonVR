cc63216b44db98a5b1a68150db7d81b0f09ef26a
Yasuyuki Someya <yasuyuki.someya@nifty.com>
2018-04-04 17:01:45 +0900

    変数名をリファクタリング

--- WatsonVR/Extension.swift
+++ WatsonVR/Extension.swift
@@ -34,9 +34,9 @@ extension UIImage {
             return self
         }
         // 80%に圧縮
-        let _size: CGSize = CGSize(width: (self.size.width * 0.8), height: (self.size.height * 0.8))
-        UIGraphicsBeginImageContext(_size)
-        self.draw(in: CGRect(x: 0, y: 0, width: _size.width, height: _size.height))
+        let size: CGSize = CGSize(width: (self.size.width * 0.8), height: (self.size.height * 0.8))
+        UIGraphicsBeginImageContext(size)
+        self.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else {
             return nil
         }
@@ -52,10 +52,10 @@ extension UIImage {
             // 最大容量の指定が無い場合はOK扱い
             return true
         }
-        guard let _data = data else {
+        guard let data = data else {
             fatalError("Data unwrap error")
         }
-        if _data.length < maxDataByte {
+        if data.length < maxDataByte {
             // 最大容量未満：OK　※以下でも良いがバッファを取ることにした
             return true
         }
